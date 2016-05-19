f475eeb6afdd6daa8e9fb68ba645b520c2874f3a
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-20 02:09:37 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/model/jdbc/JdbcCourseDao.java
+++ src/main/java/com/company/restaurant/model/jdbc/JdbcCourseDao.java
@@ -10,6 +10,20 @@ import java.util.List;
  * Created by Yevhen on 20.05.2016.
  */
 public class JdbcCourseDao extends JdbcDaoTable<Course> implements CourseDao {
+    private static final String COURSE_TABLE_NAME = "course";
+    private static final String EMPLOYEE_ID_FIELD_NAME = "employee_id";
+    private static final String POSITION_ID_FIELD_NAME = "position_id";
+    private static final String FIRST_NAME_FIELD_NAME = "first_name";
+    private static final String SECOND_NAME_FIELD_NAME = "second_name";
+    private static final String PHONE_NUMBER_FIELD_NAME = "phone_number";
+    private static final String SALARY_FIELD_NAME = "salary";
+    private static final String DEFAULT_ORDER_BY_CONDITION = "ORDER BY name";
+
+    public JdbcCourseDao() {
+        this.tableName = COURSE_TABLE_NAME;
+        this.orderByCondition = DEFAULT_ORDER_BY_CONDITION;
+    }
+
     private Course createCourse(ResultSet resultSet) {
         return null;
     }
