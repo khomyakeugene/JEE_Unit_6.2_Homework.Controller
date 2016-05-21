c41b6d449cde4a4d7edd6d701caf1c4f9488f531
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-21 18:15:02 +0300

    JEE Unit 6.2 Homework

---
+++ src/main/java/com/company/restaurant/model/jdbc/JdbcDaoTable.java
@@ -0,0 +1,73 @@
+package com.company.restaurant.model.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Yevhen on 21.05.2016.
+ */
+public abstract class JdbcDaoTable<T> extends JdbcDao<T> {
+    private static final String SQL_ALL_FIELD_OF_ALL_RECORDS = "SELECT * FROM %s";
+    private static final String SQL_ALL_FIELD_BY_FIELD_VALUE = "SELECT * FROM %s WHERE (%s = %s)";
+    private static final String SQL_ALL_FIELD_BY_TWO_FIELD_VALUE = "SELECT * FROM %s WHERE (%s = %s) AND (%s = %s)";
+    private static final String SQL_DELETE_EXPRESSION_PATTERN = "DELETE FROM %s WHERE (%s = %s)";
+
+    protected String tableName;
+    protected String orderByCondition;
+
+    private String orderByCondition() {
+        return (orderByCondition == null) ? "" : orderByCondition;
+    }
+
+    private String allQueryCondition() {
+        return String.format(SQL_ALL_FIELD_OF_ALL_RECORDS, tableName) + " " + orderByCondition();
+    }
+
+    private String fieldQueryCondition(String fieldName, Object value) {
+        return String.format(SQL_ALL_FIELD_BY_FIELD_VALUE, tableName, fieldName, JdbcDao.toString(value)) + " " +
+                orderByCondition();
+    }
+
+    private String twoFieldsQueryCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
+        return String.format(SQL_ALL_FIELD_BY_TWO_FIELD_VALUE, tableName, fieldName_1, JdbcDao.toString(value_1),
+                fieldName_2, JdbcDao.toString(value_2)) + " " + orderByCondition();
+    }
+
+    public T findObjectByFieldCondition(String fieldName, Object value) {
+        return createObjectFromQuery(fieldQueryCondition(fieldName, value));
+    }
+
+    protected List<T> findObjectsByFieldCondition(String fieldName, Object value) {
+        return createObjectListFromQuery(fieldQueryCondition(fieldName, value));
+    }
+
+    protected T findObjectByTwoFieldCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
+        return createObjectFromQuery(twoFieldsQueryCondition(fieldName_1, value_1, fieldName_2, value_2));
+    }
+
+    protected List<T> findObjectsByTwoFieldCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
+        return createObjectListFromQuery(twoFieldsQueryCondition(fieldName_1, value_1, fieldName_2, value_2));
+    }
+
+    protected List<T> findAllObjects() {
+        return createObjectListFromQuery(allQueryCondition());
+    }
+
+    protected abstract Map<String, Object> objectToDBMap(T object);
+
+    private String buildDeleteExpression(String fieldName, Object value) {
+        return String.format(SQL_DELETE_EXPRESSION_PATTERN, tableName, fieldName, JdbcDao.toString(value));
+    }
+
+    public void delRecordByFieldCondition(String fieldName, Object value) {
+        try(Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement()) {
+            statement.executeUpdate(buildDeleteExpression(fieldName, value));
+        } catch (SQLException e) {
+            databaseError(e);
+        }
+    }
+}
--- src/main/java/com/company/restaurant/model/jdbc/JdbcDaoTableWitId.java
+++ src/main/java/com/company/restaurant/model/jdbc/JdbcDaoTableWitId.java
@@ -10,62 +10,17 @@ import java.util.Map;
 /**
  * Created by Yevhen on 19.05.2016.
  */
-public abstract class JdbcDaoTableWitId<T> extends JdbcDao<T> {
+public abstract class JdbcDaoTableWitId<T> extends JdbcDaoTable<T> {
     private static final String CANNOT_GET_LAST_GENERATED_ID_PATTERN = "Add record problem: cannot get last generated %s.%s value";
     private static final String CANNOT_DELETE_RECORD_PATTERN = "Cannot delete record in table <%s> because it is impossible " +
             "to detect condition value for field <%s> nor for field <%s>";
 
-    private static final String SQL_ALL_FIELD_OF_ALL_RECORDS = "SELECT * FROM %s";
-    private static final String SQL_ALL_FIELD_BY_FIELD_VALUE = "SELECT * FROM %s WHERE (%s = %s)";
-    private static final String SQL_ALL_FIELD_BY_TWO_FIELD_VALUE = "SELECT * FROM %s WHERE (%s = %s) AND (%s = %s)";
     private static final String SQL_INSERT_EXPRESSION_PATTERN = "INSERT INTO %s (%s) VALUES(%s)";
-    private static final String SQL_DELETE_EXPRESSION_PATTERN = "DELETE FROM %s WHERE (%s = %s)";
 
-    protected String tableName;
     protected String idFieldName;
     protected String nameFieldName;
-    protected String orderByCondition;
 
     protected abstract void setId(int id, T object);
-
-    private String orderByCondition() {
-        return (orderByCondition == null) ? "" : orderByCondition;
-    }
-
-    private String allQueryCondition() {
-        return String.format(SQL_ALL_FIELD_OF_ALL_RECORDS, tableName) + " " + orderByCondition();
-    }
-
-    private String fieldQueryCondition(String fieldName, Object value) {
-        return String.format(SQL_ALL_FIELD_BY_FIELD_VALUE, tableName, fieldName, JdbcDao.toString(value)) + " " +
-                orderByCondition();
-    }
-
-    private String twoFieldsQueryCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
-        return String.format(SQL_ALL_FIELD_BY_TWO_FIELD_VALUE, tableName, fieldName_1, JdbcDao.toString(value_1),
-                fieldName_2, JdbcDao.toString(value_2)) + " " + orderByCondition();
-    }
-
-    public T findObjectByFieldCondition(String fieldName, Object value) {
-        return createObjectFromQuery(fieldQueryCondition(fieldName, value));
-    }
-
-    protected List<T> findObjectsByFieldCondition(String fieldName, Object value) {
-        return createObjectListFromQuery(fieldQueryCondition(fieldName, value));
-    }
-
-    protected T findObjectByTwoFieldCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
-        return createObjectFromQuery(twoFieldsQueryCondition(fieldName_1, value_1, fieldName_2, value_2));
-    }
-
-    protected List<T> findObjectsByTwoFieldCondition(String fieldName_1, Object value_1, String fieldName_2, Object value_2) {
-        return createObjectListFromQuery(twoFieldsQueryCondition(fieldName_1, value_1, fieldName_2, value_2));
-    }
-
-    protected List<T> findAllObjects() {
-        return createObjectListFromQuery(allQueryCondition());
-    }
-
     public T findObjectById(int id) {
         return findObjectByFieldCondition(idFieldName, id);
     }
@@ -73,8 +28,6 @@ public abstract class JdbcDaoTableWitId<T> extends JdbcDao<T> {
     public T findObjectByName(String name) {
         return findObjectByFieldCondition(nameFieldName, name);
     }
-
-    protected abstract Map<String, Object> objectToDBMap(T object);
 
     private String buildInsertExpression(T object) {
         Map<String, Object> objectToDBMap = objectToDBMap(object);
@@ -110,27 +63,6 @@ public abstract class JdbcDaoTableWitId<T> extends JdbcDao<T> {
         return result;
     }
 
-    private String buildDeleteExpression(String fieldName, Object value) {
-        return String.format(SQL_DELETE_EXPRESSION_PATTERN, tableName, fieldName, JdbcDao.toString(value));
-    }
-
-    public void delRecordByFieldCondition(String fieldName, Object value) {
-        try(Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement()) {
-            statement.executeUpdate(buildDeleteExpression(fieldName, value));
-        } catch (SQLException e) {
-            databaseError(e);
-        }
-    }
-
-    public void delRecordById(int id) {
-        delRecordByFieldCondition(idFieldName, id);
-    }
-
-    public void delRecordByName(String name) {
-        delRecordByFieldCondition(nameFieldName, name);
-    }
-
     public void delRecord(T object) {
         Map<String, Object> objectToDBMap = objectToDBMap(object);
 
@@ -146,5 +78,13 @@ public abstract class JdbcDaoTableWitId<T> extends JdbcDao<T> {
 
         // Actually, delete teh record by suitable condition
         delRecordByFieldCondition(fieldName, value);
+    }
+
+    public void delRecordById(int id) {
+        delRecordByFieldCondition(idFieldName, id);
+    }
+
+    public void delRecordByName(String name) {
+        delRecordByFieldCondition(nameFieldName, name);
     }
 }
