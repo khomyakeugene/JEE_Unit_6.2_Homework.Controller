4daa8dbef1778a3f6c75fffb6841b12d4a81bea6
khomyakeugene <khomyak_eugene@yahoo.com>
2016-05-24 01:22:06 +0300

    JEE Unit 6.2 Homework

--- src/main/java/com/company/restaurant/model/Warehouse.java
+++ src/main/java/com/company/restaurant/model/Warehouse.java
@@ -10,8 +10,11 @@ public class Warehouse {
     private String ingredientName;
     private int portionTypeId;
     private int measuringTypeId;
-
-
+    private float portionAmount;
+    private String portionDescription;
+    private String portionTypeName;
+    private String MeasuringTypeCode;
+    private String measuringTypeName;
 
     public int getIngredientId() {
         return ingredientId;
@@ -35,5 +38,69 @@ public class Warehouse {
 
     public void setAmount(float amount) {
         this.amount = amount;
+    }
+
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+    public int getPortionTypeId() {
+        return portionTypeId;
+    }
+
+    public void setPortionTypeId(int portionTypeId) {
+        this.portionTypeId = portionTypeId;
+    }
+
+    public int getMeasuringTypeId() {
+        return measuringTypeId;
+    }
+
+    public void setMeasuringTypeId(int measuringTypeId) {
+        this.measuringTypeId = measuringTypeId;
+    }
+
+    public float getPortionAmount() {
+        return portionAmount;
+    }
+
+    public void setPortionAmount(float portionAmount) {
+        this.portionAmount = portionAmount;
+    }
+
+    public String getPortionDescription() {
+        return portionDescription;
+    }
+
+    public void setPortionDescription(String portionDescription) {
+        this.portionDescription = portionDescription;
+    }
+
+    public String getPortionTypeName() {
+        return portionTypeName;
+    }
+
+    public void setPortionTypeName(String portionTypeName) {
+        this.portionTypeName = portionTypeName;
+    }
+
+    public String getMeasuringTypeCode() {
+        return MeasuringTypeCode;
+    }
+
+    public void setMeasuringTypeCode(String measuringTypeCode) {
+        MeasuringTypeCode = measuringTypeCode;
+    }
+
+    public String getMeasuringTypeName() {
+        return measuringTypeName;
+    }
+
+    public void setMeasuringTypeName(String measuringTypeName) {
+        this.measuringTypeName = measuringTypeName;
     }
 }
